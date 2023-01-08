9b28549b554cff01f34976d3821d07d7640f198f
srblodhi <sourabhlodhisrb@gmail.com>
2023-01-07 21:40:12 -0500

    added mySQL connector and changed API

--- restAPI.py
+++ restAPI.py
@@ -1,18 +1,15 @@
 from flask import Flask
-from faker import Faker
-fake = Faker()
 app = Flask(__name__)
 
 # generate some integers
 in_memory_datastore=[]
-for i in range(10000):
+for i in range(1,10001):
     in_memory_datastore.append(i)
-# for i in range(20):
-#     print(in_memory_datastore[i])
+
     
 @app.get('/')
 def list_programming_languages():
-   return {in_memory_datastore:[fake.name(),fake.address()]}
+   return in_memory_datastore
    
 #@app.route('/programming_languages/<programming_language_name>')
 #def get_programming_language(programming_language_name):
