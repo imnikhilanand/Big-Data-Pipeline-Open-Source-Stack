3f032dec9ee52634e141e92134c595de6f0fc9de
srblodhi <sourabhlodhisrb@gmail.com>
2023-01-10 02:56:38 -0500

    added API for creating, viewing, updating orders

--- src/API/restAPI.py
+++ src/API/restAPI.py
@@ -1,4 +1,15 @@
-from flask import Flask
+from flask import Flask, request
+import subprocess
+import mysql.connector
+
+mydb = mysql.connector.connect(
+  host="localhost",
+  user="root",
+  passwd="delhi_110062"
+)
+mycursor = mydb.cursor(buffered=True)
+mycursor.execute("use eCommerce;")
+
 app = Flask(__name__)
 
 # generate some integers
@@ -8,12 +19,37 @@ for i in range(1,10001):
 
     
 @app.get('/')
-def list_programming_languages():
+def home():
    return in_memory_datastore
    
-# @app.route('/order')
-# def get_order(oid, uid, pid, timestamp, status):
-#  return in_memory_datastore[programming_language_name]
+@app.route('/createorders', methods=['GET','POST'])
+def run_script():
+  script_path = r'/home/sourabhlodhisrb/Downloads/Big-Data-Pipeline-Open-Source-Stack/create_dataset/create_orders.py'
+  subprocess.call(['python3', script_path])
+  return 'Orders Created'
+
+@app.route('/updateorders', methods=['GET','POST'])
+def updateorders():
+  script_path = r'/home/sourabhlodhisrb/Downloads/Big-Data-Pipeline-Open-Source-Stack/create_dataset/update_status.py'
+  subprocess.call(['python3', script_path])
+  return 'Orders Updated'
+
+@app.get('/vieworders')
+def vieworders():
+    sql="SELECT order_id, user_id, product_id FROM orders WHERE status='0';"
+    mycursor.execute(sql)
+    result=mycursor.fetchall()
+    return result
+
+@app.get('/viewproducts')
+def viewproducts():
+    sql="SELECT * FROM products ;"
+    mycursor.execute(sql)
+    result=mycursor.fetchall()
+    return result
+
+if __name__ == '__main__':
+  app.run()
    
 
 
