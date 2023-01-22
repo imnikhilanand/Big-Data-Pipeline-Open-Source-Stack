64ca3ae0d8336fac0c411a49fdeaaee11cad3331
imnikhilanand <imnikhilanand@gmail.com>
2023-01-21 21:38:32 -0500

    Updated files and added scripts to push data from data lake to warehouse

--- src/batch/batch_processing/orders_cleaning.py
+++ src/batch/batch_processing/orders_cleaning.py
@@ -1,5 +1,6 @@
 ''' Preprocessing and migrating data from data lake to data warehouse'''
 
+# importing the libraries
 from functools import reduce
 from pyspark.sql import SparkSession
 from pyspark.sql.types import StructType
@@ -9,21 +10,45 @@ from pyspark.sql.functions import lit
 from pyspark.sql import DataFrame
 from pyspark.sql.window import Window
 
+# calling spark session
 spark = SparkSession.builder.master("local").appName("hdfs_test").getOrCreate()
- 
-#orders_schema = StructType().add("_airbyte_ab_id", "string").add("_airbyte_emitted_at", "string").add("_airbyte_data", "json")
 
-#orders_data=spark.read.csv("hdfs://localhost:9000/ecomm_data/data_lake/orders", schema=orders_schema)
+# importing all the files from the data lake  
+orders_completed_data=spark.read.text("hdfs://localhost:9000/ecomm_data/data_lake/completed_orders")
 
-orders_data = spark.read.text("src/batch/batch_processing/airbyte_test_data.csv")
+# filter all the data that have _airbyte in the rows
+orders_completed_data = orders_completed_data.filter("value not like '%_airbyte%'")
 
+# get the order_id and and completed_at
+orders_completed = orders_completed_data.withColumn('order_id', split(orders_completed_data['value'], ',').getItem(2)).withColumn('completed_at', split(orders_completed_data['value'], ',').getItem(3))
+
+# taking only order_id and completed_at timestamp
+orders_completed = orders_completed.select(orders_completed.order_id, orders_completed.completed_at)
+
+# further removing the unwanted string from the columns
+orders_completed = orders_completed.withColumn('order_id', split(orders_completed['order_id'], ':').getItem(1)).withColumn('completed_at', split(orders_completed['completed_at'], '""').getItem(3))
+
+# just for showing purpose
+orders_completed.show(truncate=False)
+
+# save the data to the warehouse
+orders_completed.repartition(5).write.mode('overwrite').option('header','true').csv('hdfs://localhost:9000/ecomm_data/data_warehouse/completed_orders')
+
+# importing all the files from the data lake  
+orders_data=spark.read.text("hdfs://localhost:9000/ecomm_data/data_lake/orders")
+
+# filter all the data that have _airbyte in the rows
 orders_data = orders_data.filter("value not like '%_airbyte%'")
 
-orders = orders_data.withColumn('order_id', split(orders_data['value'], ',').getItem(2)).withColumn('ordered_at', split(orders_data['value'], ',').getItem(3))
+# get the order_id and and completed_at
+orders = orders_data.withColumn('order_id', split(orders_data['value'], ',').getItem(2)).withColumn('user_id', split(orders_data['value'], ',').getItem(3)).withColumn('product_id', split(orders_data['value'], ',').getItem(4)).withColumn('ordered_at', split(orders_data['value'], ',').getItem(5))
 
-orders = orders.select(orders.order_id, orders.ordered_at)
+# taking only order_id and completed_at timestamp
+orders = orders.select(orders.order_id, orders.user_id, orders.product_id, orders.ordered_at)
 
-orders = orders.withColumn('order_id', split(orders['order_id'], ':').getItem(1)).withColumn('ordered_at', split(orders['ordered_at'], '""').getItem(3))
+# further removing the unwanted string from the columns
+orders = orders.withColumn('order_id', split(orders['order_id'], ':').getItem(1)).withColumn('user_id', split(orders['user_id'], ':').getItem(1)).withColumn('product_id', split(orders['product_id'], ':').getItem(1)).withColumn('ordered_at', split(orders['ordered_at'], '""').getItem(3))
 
-orders.show(truncate=False)
+# save the data to the warehouse
+orders.repartition(5).write.mode('overwrite').option('header','true').csv('hdfs://localhost:9000/ecomm_data/data_warehouse/orders')
 
